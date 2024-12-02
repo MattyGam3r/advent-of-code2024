d9ec364544d136ac3e5a6ff6cac02f64615f3608
MattyGam3r <matthew_botterill@outlook.com>
2024-12-02 19:10:54 +1030

    p2 initial

---
+++ day2/target/debug/deps/day2-8fd94bdf6f0fd4b6.d
@@ -0,0 +1,5 @@
+/home/matthew/Github/advent-of-code2024/day2/target/debug/deps/libday2-8fd94bdf6f0fd4b6.rmeta: src/main.rs
+
+/home/matthew/Github/advent-of-code2024/day2/target/debug/deps/day2-8fd94bdf6f0fd4b6.d: src/main.rs
+
+src/main.rs:
